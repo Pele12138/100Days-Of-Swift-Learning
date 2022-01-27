fe71c2928f085510869cde38f361832bce432fa1
Pele <pqq12138@outlook.com>
2022-01-27 13:01:47 +0800

    Day 1 practice

---
+++ Shared/Day1.swift
@@ -0,0 +1,50 @@
+//
+//  Day 1.swift
+//  Swift-Learing-100 (iOS)
+//
+//  Created by Pele on 2022/1/25.
+//
+
+/*import Foundation
+
+var greeting = "Hello, Pele"
+print(greeting)
+let day = "Day 1"
+
+greeting += " " + day
+print(greeting)
+//day = "Day2"
+let movie = """
+Pele
+a new
+Apple Engineer 🤓
+"""
+
+let quote = "Then he sayed \"hh\" and walked away"
+print(movie)
+print(movie.count)
+print(movie.uppercased())
+print(quote.hasSuffix("way"))
+
+print(120.isMultiple(of: 3))*/
+
+
+let dec = 0.1
+let int = 2
+//print(dec + int)
+
+let a = 1
+let b = 2.0
+print(a)
+var bool = 120.isMultiple(of:3)
+print(bool)//120 是 12 的 倍数
+bool.toggle()
+print(bool)//120 是 12 的 倍数
+
+var str = "this is a integer \(5+2)"
+var str2 = "this is a integer" + String(a)
+print(str.uppercased())
+print(str2)
+
+var celsius = 25 
+print("temperature is \(celsius)°C")
