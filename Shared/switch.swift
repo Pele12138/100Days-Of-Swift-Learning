80e3d191ae5b0496f6f2d7b17a76b16714ffdc72
Pele <pqq12138@outlook.com>
2022-01-28 13:04:46 +0800

    Swift Learning Day 5

---
+++ Shared/switch.swift
@@ -0,0 +1,20 @@
+let x = 10
+switch x {
+case 10:
+    print("10")
+    fallthrough
+case 1:
+    print("1")
+case 2:
+    print("2")
+case 3:
+    print("3")
+case 4:
+    print("4")
+case 5:
+    print("5")
+case 6:
+    print("6")
+default:
+    print("default")
+}
