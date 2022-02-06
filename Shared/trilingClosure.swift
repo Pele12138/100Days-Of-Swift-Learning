51a20d6f0757631b8e513c8cbb3e75586f42a017
Pele <pqq12138@outlook.com>
2022-02-06 21:40:52 +0800

    Move file

--- Shared/trilingClosure.swift
+++
@@ -1,8 +0,0 @@
-func animate(duration: Double ,animations: () -> Void ){
-    print("Strating a \(duration) second animation")
-    
-}
-
-animate(duration: 12.0,animations: {
-    print("Fade out")
-})
