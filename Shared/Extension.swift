c7b0887063b31ad16724f295553599fabf1521da
Pele <70554344+Pele12138@users.noreply.github.com>
2022-02-19 23:31:42 +0800

    extension

--- Shared/Extension.swift
+++ Shared/Extension.swift
@@ -9,5 +9,20 @@ var quote = " The truth is rarly pure and never simple  "
 let trimmed = quote.trimmed()
 print("\(trimmed)")
 */
-let title = " Swift 4 white spaces.. he he "
-let newString = title.trimmingCharacters(in:.whitespaces)
+// let title = " Swift 4 white spaces.. he he "
+// let newString = title.trimmingCharacters(in:.whitespaces)
+
+extension String {
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+let lyrics = """
+But I keep cruising
+Can't stop, won't stop moving
+It's like I got this music in my mind
+Saying it's gonna be alright
+"""
+
+print(lyrics.lines.count)
